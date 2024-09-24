d58a7057eae6030f2fe6ae94ed45f709bba03b3d
behzad <65810186+BehzadMahmudiazar@users.noreply.github.com>
2024-09-24 12:08:44 +0330

    Update index.php

--- Strategy/example1/index.php
+++ Strategy/example1/index.php
@@ -1,1 +1,13 @@
-e
+<?php
+
+include_once('Language.php');
+include_once('Persian.php');
+include_once('English.php');
+
+$language=new Language(new Persian());
+echo $language->getMessageWelcome();
+
+
+
+$language=new Language(new English());
+echo $language->getMessageWelcome();
