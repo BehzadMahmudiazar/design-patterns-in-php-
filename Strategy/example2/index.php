8be21fc42a2920fc47b8d66935c55527b6c14eb1
behzad <65810186+BehzadMahmudiazar@users.noreply.github.com>
2024-09-24 12:13:19 +0330

    example2

--- Strategy/example2/index.php
+++ Strategy/example2/index.php
@@ -1,1 +1,10 @@
-r
+<?php
+
+include_once('Language.php');
+include_once('Persian.php');
+include_once('English.php');
+
+$language=new Language();
+echo $language->getMessageWelcome(new Persian());
+
+echo $language->getMessageWelcome(new English());
